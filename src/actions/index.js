30cdb2d68f554275dcc78cf793a9d9da41e20648
eric nichols <ericnichols@erics-MacBook-Pro.local>
2018-12-30 08:32:03 -0500

    Latest fiies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,21 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () =>{
-    const response = await jsonPlaceholder.get('/posts')
-    return {
-        type: "FETCH_POSTS",
-        payload: response
-    }
+// We are defining a function that returns a function
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({ type:"FETCH_POSTS", payload: response.data });
 }
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type:"FETCH_USERS", payload: response.data });
+}
+
+
+// export const fetchPosts = () => {
+//     return async function(dispatch,getState){
+//         const response = await jsonPlaceholder.get('/posts');
+
+//         dispatch({ type:"FETCH_POSTS", payload: response.data });
+//     }
+// }
